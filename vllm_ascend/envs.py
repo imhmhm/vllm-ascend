93d0d0d4bfbfeb05ab83a67c3af15c657dbc758a
hengheng zhang <imhmhm@gmail.com>
2026-09-08 21:51:43 +0800

    [feat][model] Add AILabSLM model support behind AILAB_SLM_LOCAL_PATH
    Port the ailab_slm adaptation from the va_modification tree:
    - envs.py: add AILAB_SLM_LOCAL_PATH env variable
    - models/__init__.py: register AILabSLMForCausalLM only when
      AILAB_SLM_LOCAL_PATH is set
    - models/ailab_slm.py: AILabSLMForCausalLM implementation

--- vllm_ascend/envs.py
+++ vllm_ascend/envs.py
@@ -110,6 +110,9 @@ env_variables: dict[str, Callable[[], Any]] = {
     # Control the aclrtMemcpyBatchAsync compile path for KV cache offloading.
     # "1": force enable, "0": force disable, None: auto-detect from CANN headers.
     "VLLM_ASCEND_ENABLE_BATCH_MEMCPY": lambda: os.getenv("VLLM_ASCEND_ENABLE_BATCH_MEMCPY", None),
+    ## ailab_slm patch
+    "AILAB_SLM_LOCAL_PATH":
+    lambda: os.getenv("AILAB_SLM_LOCAL_PATH"),
 }
 
 # end-env-vars-definition
--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -1,5 +1,7 @@
 from vllm import ModelRegistry
 
+import vllm_ascend.envs as envs_ascend
+
 
 def register_model():
     ModelRegistry.register_model("DeepseekV4ForCausalLM", "vllm_ascend.models.deepseek_v4:AscendDeepseekV4ForCausalLM")
@@ -8,3 +10,10 @@ def register_model():
     ModelRegistry.register_model(
         "LlamaForCausalLMVwnEagle3", "vllm_ascend.models.llama_eagle3_vwn:Eagle3VwnLlamaForCausalLM"
     )
+    ## ailab_slm patch
+    if envs_ascend.AILAB_SLM_LOCAL_PATH is not None:
+        from .ailab_slm import AILabSLMForCausalLM
+        ModelRegistry.register_model(
+            "AILabSLMForCausalLM",
+            "vllm_ascend.models.ailab_slm:AILabSLMForCausalLM"
+        )
